def8e621b377ad62f1d5e9dc1087d123cd89061b
mert_gvcn <mertguvercin2942@gmail.com>
2024-04-12 00:08:30 +0300

    Test commit

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddDbContext<CommunifyContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("default"), b => b.MigrationsAssembly("CommunifyLibrary")));
 
 
-//Add builder configurations
+//Add builder configuration
 builder.AddAPI();
 builder.ConfigureAuthorization();
 builder.Services.ConfigureServices();
