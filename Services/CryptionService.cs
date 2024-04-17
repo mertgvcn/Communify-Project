08e9ee456d41653a9e75de24a1846a6097fcea2f
mert_gvcn <mertguvercin2942@gmail.com>
2024-04-17 17:10:20 +0300

    Cryption "hell no"s fixed.

--- Services/CryptionService.cs
+++ Services/CryptionService.cs
@@ -4,33 +4,27 @@ using System.Text;
 
 namespace LethalCompany_Backend.Services;
 
-public class CryptionService : ICryptionService
+public class CryptionService(IConfiguration _configuration) : ICryptionService
 {
+
     public string Decrypt(string key)
     {
-        try
+        string _key = _configuration["Crypto:key"];
+        string privatekey = _configuration["Crypto:privateKey"];
+        byte[] privatekeyByte = { };
+        privatekeyByte = Encoding.UTF8.GetBytes(privatekey);
+        byte[] _keybyte = { };
+        _keybyte = Encoding.UTF8.GetBytes(_key);
+        byte[] inputtextbyteArray = new byte[key.Replace(" ", "+").Length];
+        //This technique reverses base64 encoding when it is received over the Internet.
+        inputtextbyteArray = Convert.FromBase64String(key.Replace(" ", "+"));
+        using (DESCryptoServiceProvider dEsp = new DESCryptoServiceProvider())
         {
-            string _key = "abcdefgh";
-            string privatekey = "hgfedcba";
-            byte[] privatekeyByte = { };
-            privatekeyByte = Encoding.UTF8.GetBytes(privatekey);
-            byte[] _keybyte = { };
-            _keybyte = Encoding.UTF8.GetBytes(_key);
-            byte[] inputtextbyteArray = new byte[key.Replace(" ", "+").Length];
-            //This technique reverses base64 encoding when it is received over the Internet.
-            inputtextbyteArray = Convert.FromBase64String(key.Replace(" ", "+"));
-            using (DESCryptoServiceProvider dEsp = new DESCryptoServiceProvider())
-            {
-                var memstr = new MemoryStream();
-                var crystr = new CryptoStream(memstr, dEsp.CreateDecryptor(_keybyte, privatekeyByte), CryptoStreamMode.Write);
-                crystr.Write(inputtextbyteArray, 0, inputtextbyteArray.Length);
-                crystr.FlushFinalBlock();
-                return Encoding.UTF8.GetString(memstr.ToArray());
-            }
-        }
-        catch (Exception ex)
-        {
-            return ex.Message;
+            var memstr = new MemoryStream();
+            var crystr = new CryptoStream(memstr, dEsp.CreateDecryptor(_keybyte, privatekeyByte), CryptoStreamMode.Write);
+            crystr.Write(inputtextbyteArray, 0, inputtextbyteArray.Length);
+            crystr.FlushFinalBlock();
+            return Encoding.UTF8.GetString(memstr.ToArray());
         }
     }
 }
