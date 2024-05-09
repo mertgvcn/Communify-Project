9ab5cb2fd9063e32db5ccb965a1fbfc5366223e3
mert_gvcn <mertguvercin2942@gmail.com>
2024-05-09 23:26:49 +0300

    navbar notification icon and state added, DropDownProfile renamed to UserMenu

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import { Roles } from '../../enums/Roles';
 import { InterestViewModel } from '../../models/viewModels/InterestViewModel'
 //icons
 import { FaUserCircle } from "react-icons/fa";
+import { IoIosNotifications } from "react-icons/io";
 //helpers
 import { GetInterests } from '../../utils/apis/InterestAPI'
 //components
@@ -17,96 +18,104 @@ import SecondaryButton from '../Elements/Buttons/SecondaryButton/SecondaryButton
 import LoginForm from '../Forms/LoginForm/LoginForm'
 import RegisterForm from '../Forms/RegisterForm/RegisterForm'
 import ForgotPassword from '../Forms/ForgotPasswordForm/ForgotPassword';
-import DropDownProfile from './components/DropDownProfile/DropDownProfile'
 import Searchbar from './components/Searchbar/Searchbar';
+import UserMenu from './components/UserMenu/UserMenu';
 
 type NavbarType = {
-  role: Roles,
-  loginFormState?: boolean
+    role: Roles,
+    loginFormState?: boolean
 }
 
 const Navbar = (props: NavbarType) => {
-  const [formState, setFormState] = useState<FormStateType>({
-    loginFormState: props.loginFormState ? props.loginFormState : false,
-    registerFormState: false
-  })
-  const [forgotPasswordState, setForgotPasswordState] = useState<boolean>(false)
-  const [dropDownProfileState, setDropDownProfileState] = useState<boolean>(false)
-
-  const [interestList, setInterestList] = useState<InterestViewModel[]>([])
-
-  //functions
-  const fetchInterestList = async () => {
-    const response: any = await GetInterests()
-
-    setInterestList(response)
-  }
-
-  const handleLoginForm = () => {
-    setFormState({
-      loginFormState: true,
-      registerFormState: false
+    const [formState, setFormState] = useState<FormStateType>({
+        loginFormState: props.loginFormState ? props.loginFormState : false,
+        registerFormState: false
     })
-  }
+    const [forgotPasswordState, setForgotPasswordState] = useState<boolean>(false)
+    const [notificationState, setNotificationState] = useState<boolean>(false)
+    const [userMenuState, setUserMenuState] = useState<boolean>(false)
 
-  const handleRegisterForm = async () => {
-    await fetchInterestList()
+    const [interestList, setInterestList] = useState<InterestViewModel[]>([])
 
-    setFormState({
-      loginFormState: false,
-      registerFormState: true
-    })
-  }
+    //functions
+    const fetchInterestList = async () => {
+        const response: any = await GetInterests()
 
-  return (
-    <>
-      <nav className="navbar-wrapper">
-        <div className="navbar-components">
+        setInterestList(response)
+    }
 
-          <div className='column'>
-            <div className="navbar-logo">
-              <Link to='/' className='link'>
-                <img src={require(`../../assets/logos/large_logo.png`)} alt="img not found" />
-              </Link>
-            </div>
-          </div>
+    const handleLoginForm = () => {
+        setFormState({
+            loginFormState: true,
+            registerFormState: false
+        })
+    }
+
+    const handleRegisterForm = async () => {
+        await fetchInterestList()
+
+        setFormState({
+            loginFormState: false,
+            registerFormState: true
+        })
+    }
+
+    return (
+        <>
+            <nav className="navbar-wrapper">
+                <div className="navbar-components">
+
+                    <div className='column'>
+                        <div className="navbar-logo">
+                            <Link to='/' className='link'>
+                                <img src={require(`../../assets/logos/large_logo.png`)} alt="img not found" />
+                            </Link>
+                        </div>
+                    </div>
 
 
-          <div className='column'>
-            <div className='search-bar'>
-              <Searchbar />
-            </div>
-          </div>
+                    <div className='column'>
+                        <div className='search-bar'>
+                            <Searchbar />
+                        </div>
+                    </div>
 
 
-          <div className='column'>
-            <div className='role-based-components'>
+                    <div className='column'>
+                        <div className='role-based-components'>
 
-              {props.role == Roles.Guest.valueOf() &&
-                <div className='navbar-buttons'>
-                  <PrimaryButton value='Login' fontSize={16} width="120px" height="40px" onClickFunction={handleLoginForm} />
-                  <SecondaryButton value='Sign Up' fontSize={16} width="120px" height="40px" onClickFunction={handleRegisterForm} />
+                            {props.role == Roles.Guest.valueOf() &&
+                                <div className='navbar-buttons'>
+                                    <PrimaryButton value='Login' fontSize={16} width="120px" height="40px" onClickFunction={handleLoginForm} />
+                                    <SecondaryButton value='Sign Up' fontSize={16} width="120px" height="40px" onClickFunction={handleRegisterForm} />
+                                </div>
+                            }
+
+                            {props.role == Roles.User.valueOf() &&
+                                <>
+                                    <div className="button" style={{ fontSize: 36, marginRight: 16 }} onClick={() => { setNotificationState((prev) => !prev) }}>
+                                        <IoIosNotifications />
+                                    </div>
+                                    <div className="button" style={{ fontSize: 36 }} onClick={() => { setUserMenuState((prev) => !prev) }}>
+                                        <FaUserCircle />
+                                    </div>
+                                </>
+                            }
+
+                        </div>
+                    </div>
+
                 </div>
-              }
+            </nav >
 
-              {props.role == Roles.User.valueOf() &&
-                <div className="user" onClick={() => { setDropDownProfileState((prev) => !prev) }}>
-                  <FaUserCircle />
-                </div>
-              }
+            {formState.loginFormState && <LoginForm setFormState={setFormState} setForgotPasswordState={setForgotPasswordState} setInterestList={setInterestList} />}
+            {formState.registerFormState && <RegisterForm setFormState={setFormState} interestList={interestList} />}
+            {forgotPasswordState && <ForgotPassword setFormState={setFormState} setForgotPasswordState={setForgotPasswordState} />}
 
-            </div>
-          </div>
 
-        </div>
-      </nav >
-
-      {formState.loginFormState && <LoginForm setFormState={setFormState} setForgotPasswordState={setForgotPasswordState} setInterestList={setInterestList} />}
-      {formState.registerFormState && <RegisterForm setFormState={setFormState} interestList={interestList} />}
-      {forgotPasswordState && <ForgotPassword setFormState={setFormState} setForgotPasswordState={setForgotPasswordState} />}
-      {dropDownProfileState && <DropDownProfile setDropDownProfileState={setDropDownProfileState}/>}
-    </>
-  )
+            {userMenuState && <UserMenu setUserMenuState={setUserMenuState} />}
+        </>
+    )
 }
 
 export default Navbar
